343c7a03a9937e7c821d33545afcdbe2c5a9fb39
Molly Seither <molly.seither@centre.edu>
2018-07-13 16:26:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,14 @@ def summon_captain_planet(array)
   array.map {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  final = false
+  array.each do |name|
+    if name.length > 4
+      final = true
+    end
+  end
+  return final
 end
 
 def find_the_cheese# code an argument here
