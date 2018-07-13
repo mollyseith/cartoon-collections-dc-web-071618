b23b2a8f356f3cbfaf31b83d712cd3ae6e4be283
Molly Seither <molly.seither@centre.edu>
2018-07-13 16:27:37 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |value, index|
+    puts "#{index + 1} #{value}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  final = false
+  array.each do |name|
+    if name.length > 4
+      final = true
+    end
+  end
+  return final
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |name|
+    cheese_types.include?(name)
+  end
 end
