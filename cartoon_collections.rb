5f27b9173ae3e66a8f6cea364fcb57b18cea132a
Molly Seither <molly.seither@centre.edu>
2018-07-13 16:27:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,9 @@ def long_planeteer_calls(array)
   return final
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do 
+    cheese_types.include?(array)
+  end
 end
